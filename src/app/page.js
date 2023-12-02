9ca79db02fd7bca0dd7583437283020a0bf5e22e
Ali Anwar <alianwarofc@gmail.com>
2023-12-02 16:59:31 +0700

    boilerplate ready

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,6 @@ import { Button } from "@nextui-org/react";
 
 export default function Page() {
   return (
-    <div><Button color="danger">hey!</Button></div>
+    <div><Button color="danger">Hallo</Button></div>
   )
 }
